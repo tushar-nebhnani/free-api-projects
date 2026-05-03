e204c172969d3ad7cd34f02a6a4c88af03c6bdbf
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2026-05-03 18:17:20 +0530

    Added the Joke Project.

---
+++ jokes/script.js
@@ -0,0 +1,21 @@
+async function fetchNewJoke() {
+  UI.showLoading();
+
+  const url = "https://api.freeapi.app/api/v1/public/randomjokes/joke/random";
+  const options = { method: "GET", headers: { accept: "application/json" } };
+
+  try {
+    const res = await fetch(url, options);
+    const data = await res.json();
+    console.log(data);
+
+    UI.showJoke(data.data.content);
+  } catch (error) {
+    console.error("API Error:", error);
+    UI.showError();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", fetchNewJoke);
+
+UI.onNextJoke(fetchNewJoke);
