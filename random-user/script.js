779cb585fa30e73c13bc70b2e1c96866d374308e
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2026-05-03 19:29:34 +0530

    Random users.

--- random-user/script.js
+++ random-user/script.js
@@ -2,13 +2,13 @@ async function fetchRandomCat() {
   UI.showLoading();
 
   try {
-    const url = "https://api.freeapi.app/api/v1/public/cats/cat/random";
+    const url = "https://api.freeapi.app/api/v1/public/randomusers";
     const options = { method: "GET", headers: { accept: "application/json" } };
 
     const res = await fetch(url, options);
     const data = await res.json();
 
-    UI.showCat(data.data.image);
+    UI.showUsers(data.data.data);
   } catch (error) {
     console.error("Failed to fetch cat image:", error);
 
